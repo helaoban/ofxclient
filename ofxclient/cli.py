913740887af3a381dd7950865f0837a797ad96c7
Carlo Holl <carloholl@gmail.com>
2020-10-26 19:16:10 +0000

    Progress.

--- ofxclient/cli.py
+++ ofxclient/cli.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-from __future__ import unicode_literals
 import argparse
 import getpass
 import io
@@ -7,6 +5,7 @@ import logging
 import os
 import os.path
 import sys
+import typing as t
 
 from ofxhome import OFXHome
 
@@ -22,40 +21,54 @@ DOWNLOAD_DAYS = 30
 GlobalConfig = None
 
 
+def parse_args() -> t.Dict[str, t.Any]:
+    parser = argparse.ArgumentParser(prog="ofxclient")
+    parser.add_argument("-a", "--account")
+    parser.add_argument("-d", "--download", type=argparse.FileType("wb", 0))
+    parser.add_argument("-o", "--open", action="store_true")
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-c", "--config", help="config file path")
+    parser.add_argument(
+        "--days",
+        default=DOWNLOAD_DAYS,
+        type=int,
+        help="number of days to download (default: %s)" % DOWNLOAD_DAYS,
+    )
+    parser.add_argument(
+        "--ofx-version",
+        default=DEFAULT_OFX_VERSION,
+        type=int,
+        help="ofx version to use for new accounts (default: %s)" % DEFAULT_OFX_VERSION,
+    )
+    return vars(parser.parse_args())
+
+
 def run():
     global GlobalConfig
 
-    parser = argparse.ArgumentParser(prog='ofxclient')
-    parser.add_argument('-a', '--account')
-    parser.add_argument('-d', '--download', type=argparse.FileType('wb', 0))
-    parser.add_argument('-o', '--open', action='store_true')
-    parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('-c', '--config', help='config file path')
-    parser.add_argument('--download-days', default=DOWNLOAD_DAYS, type=int, help='number of days to download (default: %s)' % DOWNLOAD_DAYS)
-    parser.add_argument('--ofx-version', default=DEFAULT_OFX_VERSION, type=int, help='ofx version to use for new accounts (default: %s)' % DEFAULT_OFX_VERSION)
-    args = parser.parse_args()
+    args = parse_args()
 
-    if args.config:
-        GlobalConfig = OfxConfig(file_name=args.config)
+    if args["config"]:
+        GlobalConfig = OfxConfig(file_name=args["config"])
     else:
         GlobalConfig = OfxConfig()
 
     accounts = GlobalConfig.accounts()
     account_ids = [a.local_id() for a in accounts]
 
-    if args.verbose:
+    if args["verbose"]:
         logging.basicConfig(level=logging.DEBUG)
 
-    if args.download:
+    if args["download"]:
         if accounts:
-            if args.account:
-                a = GlobalConfig.account(args.account)
-                ofxdata = a.download(days=args.download_days)
+            if args["account"]:
+                a = GlobalConfig.account(args["account"])
+                ofxdata = a.download(days=args["days"])
             else:
-                ofxdata = combined_download(accounts, days=args.download_days)
-            args.download.write(ofxdata.read().encode())
-            if args.open:
-                open_with_ofx_handler(args.download.name)
+                ofxdata = combined_download(accounts, days=args["days"])
+            args["download"].write(ofxdata.read().encode())
+            if args["open"]:
+                open_with_ofx_handler(args["download"].name)
             sys.exit(0)
         else:
             print("no accounts configured")
@@ -63,55 +76,61 @@ def run():
     main_menu(args)
 
 
-def main_menu(args):
+def main_menu(args) -> None:
     while 1:
-        menu_title("Main\nEdit %s to\nchange descriptions or ofx options" %
-                   GlobalConfig.file_name)
+        menu_title(
+            "Main\nEdit %s to\nchange descriptions or ofx options"
+            % GlobalConfig.file_name
+        )
 
         accounts = GlobalConfig.accounts()
         for idx, account in enumerate(accounts):
             menu_item(idx, account.long_description())
 
-        menu_item('A', 'Add an account')
+        menu_item("A", "Add an account")
         if accounts:
-            menu_item('D', 'Download all combined')
+            menu_item("D", "Download all combined")
 
-        menu_item('Q', 'Quit')
+        menu_item("Q", "Quit")
 
         choice = prompt().lower()
-        if choice == 'a':
+        if choice == "a":
             add_account_menu(args)
-        elif choice == 'd':
+        elif choice == "d":
             if not accounts:
                 print("no accounts on file")
             else:
-                ofxdata = combined_download(accounts, days=args.download_days)
-                wrote = write_and_handle_download(
-                    ofxdata,
-                    'combined_download.ofx'
-                )
+                ofxdata = combined_download(accounts, days=args.days)
+                wrote = write_and_handle_download(ofxdata, "combined_download.ofx")
                 print("wrote: %s" % wrote)
-        elif choice in ['q', '']:
+        elif choice in ["q", ""]:
             return
         elif int(choice) < len(accounts):
             account = accounts[int(choice)]
             view_account_menu(account, args)
 
 
-def add_account_menu(args):
+def add_account_menu(args) -> None:
     menu_title("Add account")
-    while 1:
-        print('------')
-        print('Notice')
-        print('------')
-        print('You are about to search for bank connection information')
-        print('on a third party website.  This means you are trusting')
-        print('http://ofxhome.com and their security policies.')
-        print('')
-        print('You will be sending your bank name to this website.')
-        print('------')
+    while True:
+        print(
+"""
+
+------
+Notice
+------
+You are about to search for bank connection information
+on a third party website.  This means you are trusting
+http://ofxhome.com and their security policies.
+
+You will be sending your bank name to this website.
+------
+
+"""
+        )
+
         query = prompt('bank name eg. "express" (enter to exit)> ')
-        if query.lower() in ['']:
+        if not query:
             return
 
         found = OFXHome.search(query)
@@ -119,14 +138,14 @@ def add_account_menu(args):
             error("No banks found")
             continue
 
-        while 1:
+        while True:
             for idx, bank in enumerate(found):
-                menu_item(idx, bank['name'])
+                menu_item(idx, bank["name"])
             choice = prompt().lower()
-            if choice in ['q', '']:
+            if choice in ["q", ""]:
                 return
             elif int(choice) < len(found):
-                bank = OFXHome.lookup(found[int(choice)]['id'])
+                bank = OFXHome.lookup(found[int(choice)]["id"])
                 if login_check_menu(bank, args):
                     return
 
@@ -143,14 +162,14 @@ def view_account_menu(account, args):
         print("  Account Number: %s" % account.number_masked())
         print("  Institution:    %s" % institution.description)
         print("  Main Type:      %s" % str(type(account)))
-        if hasattr(account, 'routing_number'):
+        if hasattr(account, "routing_number"):
             print("  Routing Number: %s" % account.routing_number)
             print("  Sub Type:       %s" % account.account_type)
-        if hasattr(account, 'broker_id'):
+        if hasattr(account, "broker_id"):
             print("  Broker ID:      %s" % account.broker_id)
 
         print("Nerdy Info:")
-        print("  Download Up To:        %s days" % args.download_days)
+        print("  Download Up To:        %s days" % args["days"])
         print("  Username:              %s" % institution.username)
         print("  Local Account ID:      %s" % account.local_id())
         print("  Local Institution ID:  %s" % institution.local_id())
@@ -167,48 +186,47 @@ def view_account_menu(account, args):
         print("  Accept header:         %s" % client.accept)
         print("  Config File:           %s" % GlobalConfig.file_name)
 
-        menu_item('D', 'Download')
+        menu_item("D", "Download")
         choice = prompt().lower()
-        if choice == 'd':
-            out = account.download(days=args.download_days)
-            wrote = write_and_handle_download(out,
-                                              "%s.ofx" % account.local_id())
+        if choice == "d":
+            out = account.download(days=args["days"])
+            wrote = write_and_handle_download(out, "%s.ofx" % account.local_id())
             print("wrote: %s" % wrote)
         return
 
 
 def login_check_menu(bank_info, args):
-    print('------')
-    print('Notice')
-    print('------')
-    print('You are about to test to make sure your username and password')
-    print('are correct.  This means you will be sending it to the URL below.')
-    print('If the URL does not appear to belong to your bank then you should')
-    print('exit this program by hitting CTRL-C.')
-    print('  bank name: %s' % (bank_info['name']))
-    print('  bank url:  %s' % (bank_info['url']))
-    print('------')
+    print("------")
+    print("Notice")
+    print("------")
+    print("You are about to test to make sure your username and password")
+    print("are correct.  This means you will be sending it to the URL below.")
+    print("If the URL does not appear to belong to your bank then you should")
+    print("exit this program by hitting CTRL-C.")
+    print("  bank name: %s" % (bank_info["name"]))
+    print("  bank url:  %s" % (bank_info["url"]))
+    print("------")
     while 1:
-        username = ''
+        username = ""
         while not username:
-            username = prompt('username> ')
+            username = prompt("username> ")
 
-        password = ''
-        prompt_text = 'password> '
-        if os.name == 'nt' and sys.version_info < (3, 0):
-            prompt_text = prompt_text.encode('utf8')
+        password = ""
+        prompt_text = "password> "
+        if os.name == "nt" and sys.version_info < (3, 0):
+            prompt_text = prompt_text.encode("utf8")
         while not password:
             password = getpass.getpass(prompt=prompt_text)
 
         i = Institution(
-            id=bank_info['fid'],
-            org=bank_info['org'],
-            url=bank_info['url'],
-            broker_id=bank_info['brokerid'],
-            description=bank_info['name'],
+            id=bank_info["fid"],
+            org=bank_info["org"],
+            url=bank_info["url"],
+            broker_id=bank_info["brokerid"],
+            description=bank_info["name"],
             username=username,
             password=password,
-            client_args=client_args_for_bank(bank_info, args.ofx_version)
+            client_args=client_args_for_bank(bank_info, args["ofx_version"]),
         )
         try:
             i.authenticate()
@@ -238,18 +256,19 @@ def client_args_for_bank(bank_info, ofx_version):
     :return: Client arguments for a specific institution
     :rtype: dict
     """
-    client_args = {'ofx_version': str(ofx_version)}
-    if 'ofx.discovercard.com' in bank_info['url']:
+    client_args = {"ofx_version": str(ofx_version)}
+    if "ofx.discovercard.com" in bank_info["url"]:
         # Discover needs no User-Agent and no Accept headers
-        client_args['user_agent'] = False
-        client_args['accept'] = False
-    if 'www.accountonline.com' in bank_info['url']:
+        client_args["user_agent"] = False
+        client_args["accept"] = False
+    if "www.accountonline.com" in bank_info["url"]:
         # Citi needs no User-Agent header
-        client_args['user_agent'] = False
+        client_args["user_agent"] = False
     return client_args
 
+
 def write_and_handle_download(ofx_data, name):
-    outfile = io.open(name, 'w')
+    outfile = io.open(name, "w")
     outfile.write(ofx_data.read())
     outfile.close()
     if AUTO_OPEN_DOWNLOADS:
@@ -257,7 +276,7 @@ def write_and_handle_download(ofx_data, name):
     return os.path.abspath(name)
 
 
-def prompt(text='choice> '):
+def prompt(text="choice> "):
     try:
         # python 2
         got = raw_input(text)
@@ -267,7 +286,7 @@ def prompt(text='choice> '):
     return got
 
 
-def error(text=''):
+def error(text=""):
     print("!! %s" % text)
 
 
@@ -283,14 +302,16 @@ def menu_title(name):
 
 def open_with_ofx_handler(filename):
     import platform
+
     sysname = platform.system()
-    if sysname == 'Darwin':
+    if sysname == "Darwin":
         os.system("/usr/bin/open '%s'" % filename)
-    elif sysname == 'Windows':
+    elif sysname == "Windows":
         os.startfile(filename)
     else:
         # linux
         os.system("xdg-open '%s'" % filename)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
--- ofxclient/client.py
+++ ofxclient/client.py
@@ -9,11 +9,11 @@ import typing as t
 if t.TYPE_CHECKING:
     from http.client import HTTPResponse
 
-DEFAULT_APP_ID = 'QWIN'
-DEFAULT_APP_VERSION = '2500'
-DEFAULT_OFX_VERSION = '102'
-DEFAULT_USER_AGENT = 'httpclient'
-DEFAULT_ACCEPT = '*/*, application/x-ofx'
+DEFAULT_APP_ID = "QWIN"
+DEFAULT_APP_VERSION = "2500"
+DEFAULT_OFX_VERSION = "102"
+DEFAULT_USER_AGENT = "httpclient"
+DEFAULT_ACCEPT = "*/*, application/x-ofx"
 
 DEFAULT_HEADERS = {
     "Accept": DEFAULT_ACCEPT,
@@ -53,48 +53,38 @@ class Client:
     def __init__(
         self,
         institution,
-        id=ofx_uid(),
+        client_id: t.Optional[str] = None,
         app_id=DEFAULT_APP_ID,
         app_version=DEFAULT_APP_VERSION,
         ofx_version=DEFAULT_OFX_VERSION,
         user_agent=DEFAULT_USER_AGENT,
-        accept=DEFAULT_ACCEPT
-    ):
+        accept=DEFAULT_ACCEPT,
+    ) -> None:
         self.institution = institution
-        self.id = id
+        self.client_id = client_id or ofx_uid()
         self.app_id = app_id
         self.app_version = app_version
         self.ofx_version = ofx_version
         self.user_agent = user_agent
         self.accept = accept
         # used when serializing Institutions
-        self._init_args = {
-            'id': self.id,
-            'app_id': self.app_id,
-            'app_version': self.app_version,
-            'ofx_version': self.ofx_version,
-            'user_agent': self.user_agent,
-            'accept': self.accept
-        }
         self.cookie = 3
 
-    @property
-    def init_args(self):
-        """
-        Return a dict of the arguments used to initialize this client,
-        suitable for use when serializing an Institution.
-
-        :return: constructor arguments
-        :rtype: dict
-        """
-        return self._init_args
+        self.init_args = {
+            "id": self.client_id,
+            "app_id": self.app_id,
+            "app_version": self.app_version,
+            "ofx_version": self.ofx_version,
+            "user_agent": self.user_agent,
+            "accept": self.accept,
+        }
 
     def authenticated_query(
         self,
-        with_message=None,
-        username=None,
-        password=None
-    ):
+        with_message: t.Optional[str] = None,
+        username: t.Optional[str] = None,
+        password: t.Optional[str] = None,
+    ) -> str:
         """Authenticated query
 
         If you pass a 'with_messages' array those queries will be passed along
@@ -103,7 +93,7 @@ class Client:
         u = username or self.institution.username
         p = password or self.institution.password
 
-        contents = ['OFX', self._sign_on(username=u, password=p)]
+        contents = ["OFX", self._sign_on(username=u, password=p)]
         if with_message:
             contents.append(with_message)
         return LINE_ENDING.join([self.header(), _tag(*contents)])
@@ -120,9 +110,10 @@ class Client:
 
     def brokerage_account_query(self, number, date, broker_id):
         return self.authenticated_query(
-            self._investment_request(broker_id, number, date))
+            self._investment_request(broker_id, number, date)
+        )
 
-    def account_list_query(self, date='19700101000000'):
+    def account_list_query(self, date="19700101000000"):
         return self.authenticated_query(self._account_request(date))
 
     def post(self, query):
@@ -131,11 +122,13 @@ class Client:
         sending back session cookies (``self.set_cookies`` True).
         """
         res, response = self._do_post(query)
-        cookies = res.getheader('Set-Cookie', None)
+        cookies = res.getheader("Set-Cookie", None)
         if len(response) == 0 and cookies is not None and res.status == 200:
-            logging.debug('Got 0-length 200 response with Set-Cookies header; '
-                          'retrying request with cookies')
-            _, response = self._do_post(query, [('Cookie', cookies)])
+            logging.debug(
+                "Got 0-length 200 response with Set-Cookies header; "
+                "retrying request with cookies"
+            )
+            _, response = self._do_post(query, [("Cookie", cookies)])
         return response
 
     def _do_post(self, query, extra_headers=[]) -> t.Tuple[HTTPResponse, str]:
@@ -150,14 +143,13 @@ class Client:
         :return: 2-tuple of (HTTPResponse, str response body)
         :rtype: tuple
         """
-        logging.debug('posting data to %s' % self.institution.url)
+        logging.debug("posting data to %s" % self.institution.url)
         garbage, path = splittype(self.institution.url)
         host, selector = splithost(path)
         h = HTTPSConnection(host, timeout=60)
         # Discover requires a particular ordering of headers, so send the
         # request step by step.
-        h.putrequest('POST', selector, skip_host=True,
-                     skip_accept_encoding=True)
+        h.putrequest("POST", selector, skip_host=True, skip_accept_encoding=True)
 
         headers = {
             **DEFAULT_HEADERS,
@@ -165,20 +157,20 @@ class Client:
                 "Host": host,
                 "Content-Length": len(query),
             }
-            **extra_headers,
+            ** extra_headers,
         }
-        logging.debug('---- request headers ----')
+        logging.debug("---- request headers ----")
         for name, value in headers.items():
-            logging.debug('%s: %s', name, value)
+            logging.debug("%s: %s", name, value)
             h.putheader(name, value)
-        logging.debug('---- request body (query) ----')
+        logging.debug("---- request body (query) ----")
         logging.debug(query)
         h.endheaders(query.encode())
         response = h.getresponse()
-        decoded = response.read().decode('ascii', 'ignore')
-        logging.debug('---- response ----')
+        decoded = response.read().decode("ascii", "ignore")
+        logging.debug("---- response ----")
         logging.debug(response.__dict__)
-        logging.debug('Headers: %s', response.getheaders())
+        logging.debug("Headers: %s", response.getheaders())
         logging.debug(decoded)
         response.close()
         return response, decoded
@@ -197,21 +189,19 @@ class Client:
             "CHARSET:1252",
             "COMPRESSION:NONE",
             "OLDFILEUID:NONE",
-            "NEWFILEUID:"+ofx_uid(),
-            ""
+            "NEWFILEUID:" + ofx_uid(),
+            "",
         ]
         return LINE_ENDING.join(parts)
 
-    """Generate signon message"""
-    def _sign_on(self, username=None, password=None):
-        u = username or self.institution.username
-        p = password or self.institution.password
+    def _sign_on(self, username: str, password: str) -> str:
+        """Generate signon message"""
         fidata = [_field("ORG", self.institution.org)]
         if self.institution.id:
             fidata.append(_field("FID", self.institution.id))
 
-        if str(self.ofx_version) == '103':
-            client_uid = _field('CLIENTUID', self.id)
+        if str(self.ofx_version) == "103":
+            client_uid = _field("CLIENTUID", self.client_id)
         else:
             client_uid = ""
 
@@ -219,15 +209,15 @@ class Client:
             "SIGNONMSGSRQV1",
             _tag(
                 "SONRQ",
-                 _field("DTCLIENT", now()),
-                 _field("USERID", u),
-                 _field("USERPASS", p),
-                 _field("LANGUAGE", "ENG"),
-                 _tag("FI", *fidata),
-                 _field("APPID", self.app_id),
-                 _field("APPVER", self.app_version),
-                 client_uid
-            )
+                _field("DTCLIENT", now()),
+                _field("USERID", username),
+                _field("USERPASS", password),
+                _field("LANGUAGE", "ENG"),
+                _tag("FI", *fidata),
+                _field("APPID", self.app_id),
+                _field("APPVER", self.app_version),
+                client_uid,
+            ),
         )
 
     def _account_request(self, dtstart):
@@ -237,22 +227,24 @@ class Client:
     # this is from _credit_card_request below and reading
     # page 176 of the latest OFX doc.
     def _bare_request(self, acctid, dtstart, accttype, bankid):
-        req = _tag("STMTRQ",
-                   _tag("BANKACCTFROM",
-                        _field("BANKID", bankid),
-                        _field("ACCTID", acctid),
-                        _field("ACCTTYPE", accttype)),
-                   _tag("INCTRAN",
-                        _field("DTSTART", dtstart),
-                        _field("INCLUDE", "Y")))
+        req = _tag(
+            "STMTRQ",
+            _tag(
+                "BANKACCTFROM",
+                _field("BANKID", bankid),
+                _field("ACCTID", acctid),
+                _field("ACCTTYPE", accttype),
+            ),
+            _tag("INCTRAN", _field("DTSTART", dtstart), _field("INCLUDE", "Y")),
+        )
         return self._message("BANK", "STMT", req)
 
     def _credit_card_request(self, acctid, dtstart):
-        req = _tag("CCSTMTRQ",
-                   _tag("CCACCTFROM", _field("ACCTID", acctid)),
-                   _tag("INCTRAN",
-                        _field("DTSTART", dtstart),
-                        _field("INCLUDE", "Y")))
+        req = _tag(
+            "CCSTMTRQ",
+            _tag("CCACCTFROM", _field("ACCTID", acctid)),
+            _tag("INCTRAN", _field("DTSTART", dtstart), _field("INCLUDE", "Y")),
+        )
         return self._message("CREDITCARD", "CCSTMT", req)
 
     def _investment_request(
@@ -266,31 +258,33 @@ class Client:
             _tag(
                 "INVACCTFROM",
                 _field("BROKERID", broker_id),
-                _field("ACCTID", account_id)),
-               _tag("INCTRAN",
-                    _field("DTSTART", start_date),
-                    _field("INCLUDE", "Y")),
-               _field("INCOO", "Y"),
-               _tag("INCPOS",
-                    _field("DTASOF", now()),
-                    _field("INCLUDE", "Y")),
-               _field("INCBAL", "Y"))
+                _field("ACCTID", account_id),
+            ),
+            _tag("INCTRAN", _field("DTSTART", start_date), _field("INCLUDE", "Y")),
+            _field("INCOO", "Y"),
+            _tag("INCPOS", _field("DTASOF", now()), _field("INCLUDE", "Y")),
+            _field("INCBAL", "Y"),
+        )
         return self._message("INVSTMT", "INVSTMT", req)
 
     def _message(self, msgType, trnType, request) -> str:
-        return _tag(msgType+"MSGSRQV1",
-                    _tag(trnType+"TRNRQ",
-                         _field("TRNUID", ofx_uid()),
-                         _field("CLTCOOKIE", self.next_cookie()),
-                         request))
+        return _tag(
+            msgType + "MSGSRQV1",
+            _tag(
+                trnType + "TRNRQ",
+                _field("TRNUID", ofx_uid()),
+                _field("CLTCOOKIE", self.next_cookie()),
+                request,
+            ),
+        )
 
 
 def _field(tag: str, value: str) -> str:
-    return "<"+tag+">"+value
+    return "<" + tag + ">" + value
 
 
 def _tag(tag: str, *contents: str) -> str:
-    return LINE_ENDING.join(['<'+tag+'>']+list(contents)+['</'+tag+'>'])
+    return LINE_ENDING.join(["<" + tag + ">"] + list(contents) + ["</" + tag + ">"])
 
 
 def now():
